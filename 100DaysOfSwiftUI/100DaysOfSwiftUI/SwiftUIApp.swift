3b6c4a12bf108884985d2e654084e28326bbe634
vaishnavi <vaishnaviwaghule3421@gmail.com>
2024-01-15 16:57:08 +0530

    Project Setup

---
+++ 100DaysOfSwiftUI/100DaysOfSwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DaysOfSwiftUIApp.swift
+//  100DaysOfSwiftUI
+//
+//  Created by Sanjay, Vaishnavi on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
